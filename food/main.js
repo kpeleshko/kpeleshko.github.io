bb33f4fadb5d11257165c0e708b43dd8a9703955
kpeleshko <kate200ka@gmail.com>
2020-01-22 10:15:09 +0200

    NEW: website for restaurant

---
+++ food/main.js
@@ -0,0 +1,14 @@
+$('.banner').slick({
+    focusOnSelect: true,
+    slidesToShow: 1,
+    autoplay: true,
+    dots: true,
+  });
+  
+  $('.about-section__photos').slick({
+    focusOnSelect: true,
+    slidesToShow: 1,
+    autoplay: true,
+    dots: true,
+  });
+
