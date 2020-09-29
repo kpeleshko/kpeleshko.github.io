17c4e2df74950d0ea715e13a220c08198cb0690c
Kate Peleshko <kate200ka@gmail.com>
2020-09-29 16:28:23 +0300

    NEW: New react app added.

---
+++ notes/precache-manifest.14fc1b501b19ba59dd19cb38df30705c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334907f18e9b4020def59e44f0aa5297",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffeed9ec0b8ba83f51ff",
+    "url": "/static/css/main.8444ec8c.chunk.css"
+  },
+  {
+    "revision": "56671e4be5f40bad7f2c",
+    "url": "/static/js/2.bddcc5e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bddcc5e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffeed9ec0b8ba83f51ff",
+    "url": "/static/js/main.0b8b32be.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
